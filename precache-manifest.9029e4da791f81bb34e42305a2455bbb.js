3c78f405b938638b5ee664b0aea79ecf62731061
BlackDrake <wwandyww@yandex.ru>
2019-04-02 18:32:26 +0300

    Updates

---
+++ precache-manifest.9029e4da791f81bb34e42305a2455bbb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99a9107ec23efd1b367",
+    "url": "/WeatherApp/static/js/main.e99a9107.chunk.js"
+  },
+  {
+    "revision": "9d064671a7eaa16acd55",
+    "url": "/WeatherApp/static/js/1.9d064671.chunk.js"
+  },
+  {
+    "revision": "a65dfe9f73dd55aa5b62",
+    "url": "/WeatherApp/static/js/runtime~main.a65dfe9f.js"
+  },
+  {
+    "revision": "98e202853bf1af980c530d327fa41b34",
+    "url": "/WeatherApp/index.html"
+  }
+];
